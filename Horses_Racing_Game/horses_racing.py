26c3438d3c8bffcfd7526c5b9c1044fcbf19f747
omarali1997 <omar.aziz.9718@gmail.com>
2022-10-14 18:37:10 +0000

    v3

--- Horses_Racing_Game/horses_racing.py
+++ Horses_Racing_Game/horses_racing.py
@@ -1,135 +1,116 @@
 import random
-
-from classes import HorsesRacing
 from time import sleep
+from classes import HorsesRacing
 
 
 def random_contender():
-        contender={
-        "name":['Kind','Alex', 'Lilly','Sugar','Lady','Cash','Lucky','Kind','Chance','Rose',],
-        "Breed":["american", "arabian", "thoroughbred",'gypsy','andalusian'],
-        "Point":[50,60,70,80,90,100]
-        }
-        return [random.choice(contender['name']),random.choice(contender['Breed']),random.choice(contender['Point'])]
 
+    '''
+    Function choose contender randomly depand on dictionary
+    '''
 
+    contender={
+    "name":['Kind','Alex', 'Lilly','Sugar','Lady','Cash','Lucky','Kind','Chance','Rose',],
+    "Breed":["american", "arabian", "thoroughbred",'gypsy','andalusian'],
+    "Point":[50,60,70,80,90,100]
+    }
+    return [random.choice(contender['name']),random.choice(contender['Breed']),random.choice(contender['Point'])]
+
+def draw_horse():
+    a = """
+    *******************************************
+    welcome to             O  ,--.
+    Horse Racing Game   _ /|\/  /\|
+                    ,;( )__7,  )
+                    /  //  // '--.
+                    '    \,   ,' '
+    *******************************************
+    """
+
+    b = """
+    ************************************************
+    Thank you for playing       O  ,--.
+    Horse Racing Game        _ /|\/  /\|
+                         ,;( )__7,  )
+                         /  //  // '--.
+                         '    \,   ,' '
+    ************************************************
+    Created By : 
+                  *****************     ************
+                  * Amani_ALZoubi *     * Omar_Ali *  
+                  *****************     ************
+
+    ************************************************
+
+    """
+    return a,b
+       
 if __name__ == "__main__":
 
-        
+    print(draw_horse()[0])
+    horse = HorsesRacing()
+    contender = HorsesRacing()
+    horse.set_name(input("Enter your Horse name => "))
+    lis=["american", "arabian", "thoroughbred", "gypsy", "andalusian"]
+    e=input('choose his Breed : \n  american   arabian   thoroughbred    gypsy    andalusian \n => ')     
+    i=0
 
-    
+                        # #################### Start The Game #################### #
 
+    while  i == 0 :
+            if e not in lis :
+                    print("!!!!!!!!!!!!!!!!!!!! WRONG CHOICE !!!!!!!!!!!!!!!!!!!!")
+                    e=input('choose his Breed again : \n  american   arabian   thoroughbred    gypsy    andalusian \n => ')
+            else:
+                    horse.set_Breed(e)
+                    x= horse.get_name()
+                    lose=f"{x} fainted!"
+                    contender.set_name(random_contender()[0])
+                    contender.set_Breed(random_contender()[1])
+                    contender.set_point(random_contender()[2])
+                    print("\n******************************************************************************")  
+                    print(f"\n           {horse} ** VS ** {contender}  \n")
+                    print("*******************************************************************************\n")
+                    hr=horse.racing(contender)
+                    rr=f"ROUND {HorsesRacing.get_round_numbers()}"
+                    sleep(1)  
 
+                        # #################### Create Load Bar #################### #
 
-    
+                    print(f"####################################################################### {rr} ###########################################\n")
+                    def Loadbar(iteration, total,prefix='', suffix='', decimals=1, length=100, fill='>'   ):
+                            percent = ('{0:.'+ str(decimals) + 'f}').format(100 * (iteration/float(total)))
+                            filledLenength = int(length * iteration // total)
+                            bar = fill * filledLenength + '_' * (length - filledLenength)
+                            print(f'\r{prefix} | {bar} | {percent}% {suffix}', end='\r')
+                            if iteration == total:
+                                    print()
 
-#     print(HorsesRacing.get_round_numbers())
-#     j=f"{x} and {y} battled hard. It's a tie."
+                    items = list(range(0, 50))
+                    l = len(items)  
+                    Loadbar(0,l,prefix=rr, suffix='Complete', length=l)
+                    for i, item in enumerate(items):
+                            sleep(0.04)
+                            Loadbar(i + 1, l, prefix=rr, suffix='Complete', length=l)
+                    print(f"\n {hr}")
+                    print(f"\n####################################################################### {rr} ###########################################\n")
+                    print(f" {horse} ")
+                    print(f" {contender} ")
+                    print(f" ROUND {HorsesRacing.get_round_numbers()} ")
 
-#     if (horse.racing(contender)) == g:
-        # print(horse.racing(contender))
-#     elif (horse.racing(contender)) == h:
-        # print("do you want to play again ?")
+                        # #################### check if user want to continue or not #################### #
 
-#     elif (horse.racing(contender)) == j:
-        # print(horse.racing(contender))
-#     print(horse.racing(contender))
+                    if hr == lose:
+                            print(f" \n OoOh NoOoO >_< {horse.get_name()} fainted \n\n ############################# \n")
+                            w=input("Do You Want To Play Again ?! [y/n] ==> ")
 
-        horse = HorsesRacing()
-        contender = HorsesRacing()
-        horse.set_name(input("Enter your Horse name => "))
-        lis=["american", "arabian", "thoroughbred", "gypsy", "andalusian"]
-        e=input('choose his Breed : \n  american   arabian   thoroughbred    gypsy    andalusian \n => ')
-        
-
-        
-        i=0
-        while  i != 'q':
-                if e not in lis :
-                        print("!!!!!!!!!!!!!!!!!!!! WRONG CHOICE !!!!!!!!!!!!!!!!!!!!")
-                        e=input('choose his Breed again : \n  american   arabian   thoroughbred    gypsy    andalusian \n => ')
-                else:
-                        horse.set_Breed(e)
-                        x= horse.get_name()
-                        lose=f"{x} fainted!"
-                        contender.set_name(random_contender()[0])
-                        contender.set_Breed(random_contender()[1])
-                        contender.set_point(random_contender()[2])  
-                        # print(horse.racing(contender))
-                        rr=f"round {HorsesRacing.get_round_numbers()}"
-                        # ''''
-
-                        print(f"####################################################################### {rr} ###########################################")
-                        def Loadbar(iteration, total,prefix='', suffix='', decimals=1, length=100, fill='>'   ):
-                                percent = ('{0:.'+ str(decimals) + 'f}').format(100 * (iteration/float(total)))
-                                filledLenength = int(length * iteration // total)
-                                bar = fill * filledLenength + '_' * (length - filledLenength)
-                                print(f'\r{prefix} | {bar} | {percent}% {suffix}', end='\r')
-                                if iteration == total:
-                                        print()
-
-                        items = list(range(0, 50))
-                        l = len(items)  
-                        Loadbar(0,l,prefix=rr, suffix='Complete', length=l)
-                        for i, item in enumerate(items):
-                                sleep(0.03)
-                                Loadbar(i + 1, l, prefix=rr, suffix='Complete', length=l)
-                        print(f"####################################################################### {rr} ###########################################")
-
-                        # '''
-                        print(horse)
-                        print(contender)
-                        print(f"round {HorsesRacing.get_round_numbers()}")
-
-                        if (horse.racing(contender)) == lose:
-                                print(f"ooh no :( {horse.racing(contender)}")
-                                w=input("do you need to play again? [y/n] ==> ")
-                                if w == 'y' or w== 'Y' :
-                                        i=0
-                                elif w == 'n' or w== 'N' : 
-                                        print(rr)
-                                        print("***********************Thank you for playing*************************")
-                                        i='q'    
-                        else:
+                            while w not in ['y','Y','N','n']:
+                               w=input(">>>> Please Choose [y/n] ==> ")  
+                            if w == 'y' or w== 'Y' :
                                 i=0
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-'''
-
-# ========================================================================================================
-    def Loadbar(iteration, total,prefix='', suffix='', decimals=1, length=100, fill='>'   ):
-            percent = ('{0:.'+ str(decimals) + 'f}').format(100 * (iteration/float(total)))
-            filledLenength = int(length * iteration // total)
-            bar = fill * filledLenength + '_' * (length - filledLenength)
-            print(f'\r{prefix} | {bar} | {percent}% {suffix}', end='\r')
-            if iteration == total:
-                    print()
-    
-    items = list(range(0, 50))
-    l = len(items)  
-    Loadbar(0,l,prefix='progress:', suffix='Complete', length=l)
-    for i, item in enumerate(items):
-            sleep(0.05)
-            Loadbar(i + 1, l, prefix='progress:', suffix='Complete', length=l)
-    for i, item in enumerate(items):
-            sleep(0.05)
-            Loadbar(i + 1, l, prefix='progress:', suffix='Complete', length=l)
-# ========================================================================================================
-
-'''
+                            elif w == 'n' or w== 'N' : 
+                                print(rr)
+                                print(draw_horse()[1])
+                                i=1  
+                    else:
+                            i=0
--- tests/test_horses.py
+++ tests/test_horses.py
@@ -1,8 +1,9 @@
 import pytest
+
 from Horses_Racing_Game.classes import HorsesRacing
 
-
 # #################### Tests #################### #
+
 def test_horse_name(horse):
     kind = horse.set_name("Kind")
     assert horse.get_name() == "Kind"
@@ -15,27 +16,57 @@ def test_horse_point(horse):
     horse_point = horse.set_point(40)
     assert horse.get_point() == 40
 
-def test_horse_str(horse):
+def test_contender_str(contender):
+    assert str(contender) == "king (arabian): 70/100"
+
+def test_horse_str_repr():
     kind=HorsesRacing()
     kind.set_name("Kind")
     kind.set_Breed("american")
     kind.set_point(40)
     assert str(kind) == "Kind (american): 40/100"
-
-def test_horse_repr(horse):
-    kind=HorsesRacing()
-    kind.set_name("Kind")
-    kind.set_Breed("american")
-    kind.set_point(40)
     assert repr(kind) == "name= Kind, Breed= american point= 40, high_point= 100"
-    
+
+
+def test_tie(horse,contender):
+    horse.set_name("Sugar")
+    horse.set_Breed("arabian")
+    assert horse.racing(contender)=="Sugar and king battled hard. It's a tie."
+    assert horse.get_point()==40
+    assert contender.get_point()==60
+
+
+def test_win(horse,contender):
+    horse.set_name("Lucky")
+    horse.set_Breed("american")
+    assert horse.racing(contender)=="Lucky won. Congratulations!"
+    assert horse.get_point()==60
+    assert contender.get_point()==0
+
+def test_lose(horse,contender):
+    horse.set_name("Rose")
+    horse.set_Breed("thoroughbred")
+    assert horse.racing(contender)=="Rose fainted!"
+    assert horse.get_point()==0
+    assert contender.get_point()==80
+
+def test_round_numbers(horse,contender):
+    horse.set_name("Rose")
+    horse.set_Breed("thoroughbred")
+    horse.racing(contender)
+    assert HorsesRacing.get_round_numbers()==4
 
 # #################### Fixtures #################### # 
 
 @pytest.fixture
 def horse():
-    horse1 = HorsesRacing()
-    return horse1
+    horse = HorsesRacing()
+    return horse
 
+@pytest.fixture
 def contender():
-    pass
+    king = HorsesRacing()
+    king.set_name("king")
+    king.set_Breed("arabian")
+    king.set_point(70)
+    return king
